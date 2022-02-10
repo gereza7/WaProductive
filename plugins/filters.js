0404a7542fb021d06d9ec56433a956585e0bbba9
gereza7 <gereza7@gmail.com>
2022-02-10 00:14:19 +0000

    date update to spanish

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import moment from 'moment'
+import 'moment/locale/es'
 
 Vue.filter('formatDate', (value) => {
   if (!value) return ''
-  return moment(value).format('dddd, MMMM Do YYYY')
+  return moment(value).format('dddd, D MMMM  YYYY')
 })
